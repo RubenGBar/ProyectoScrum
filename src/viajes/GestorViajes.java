ce8434cba15ef65b35b480819f87e71e4f25a5bf
Alonso Fernández <alonso.fernandez@iesnervion.es>
2024-05-10 14:37:43 +0200

    Commented

--- src/viajes/GestorViajes.java
+++ src/viajes/GestorViajes.java
@@ -3,6 +3,7 @@ package viajes;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -13,7 +14,7 @@ public class GestorViajes {
 	/**
 	 * Almacena todos los viajes del fichero
 	 */
-	public ArrayList<Viaje> listaViajes;
+	public ArrayList<Viaje> listaViajes = new ArrayList<>();
 	
 	/**
 	 * Se emplea para cargar los datos del fichero
@@ -22,17 +23,22 @@ public class GestorViajes {
 	
 	public void preparar() {
 		String linea;
+		// almacena los valores almacenados en una línea temporalmente
 		String[] viajeString;
 		try {
+			// acceso al fichero
 			br = new BufferedReader(new FileReader("src/viajes/datosTurismo.txt"));
+			// recorremos el fichero y vamos agregando los viajes a listaViajes
 			while((linea = br.readLine()) != null) {
 				viajeString = linea.split("::");
-				Viaje viaje = new Viaje(viajeString[0], viajeString[1], viajeString[2]);
+				Viaje viaje = new Viaje(viajeString[0], viajeString[1], Integer.parseInt(viajeString[2]));
 				listaViajes.add(viaje);
 			}
 			
 			br.close();
-		} catch (Exception e) {
+		} catch (FileNotFoundException e) {
+			System.err.println("No se ha encontrado el fichero de viajes.");
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
