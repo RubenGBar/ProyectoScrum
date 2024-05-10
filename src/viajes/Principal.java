4203fd1ece1f54e1bf4534aa183b3737088f9859
RubenGBar <ruben.garcia@iesnervion.es>
2024-05-10 13:56:32 +0200

    Primer Commit

---
+++ src/viajes/Principal.java
@@ -0,0 +1,5 @@
+package viajes;
+
+public class Principal {
+
+}
---
+++ src/viajes/Viaje.java
@@ -0,0 +1,17 @@
+package viajes;
+
+public class Viaje {
+	
+	String lugar = "";
+	
+	String fecha = "";
+	
+	int precio = 0;
+	
+	public Viaje(String lugar, String fecha, int precio) {
+		if(lugar != null && !lugar.equals(fecha)) {
+			
+		}
+	}
+
+}
