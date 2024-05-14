ddec6d2959b12c25675e5cd8a2a3bec5c7b487a8
nestor <nestor@TUF-B>
2024-05-14 11:28:52 +0200

    Clase Principal terminada a nivel del Sprint 1

--- src/viajes/Principal.java
+++ src/viajes/Principal.java
@@ -1,46 +1,98 @@
 package viajes;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
+		// GestorViajes gv: crea un nuevo objeto GestorViajes
+		GestorViajes gv = new GestorViajes();
+		
+		// String lugar: lugar de uin viaje
+		String lugar;
+		
+		// String fecha: fecha de un viaje
+		String fecha;
+		
+		// int precio: precio de un viaje
+		int precio;
+		
+		// int opcion: opcion seleccionada por el usuario
 		int opcion = 0;
 		
+		// Inicia el Scanner
 		Scanner sc = new Scanner(System.in);
 		
+		// Do-While: mientras la opcion no sea 5, el programa se seguira ejecutando
 		do {
+			
+			// Muestra el menu y pide al usuario una opcion
 			mostrarMenu();
 			opcion = sc.nextInt();
 			
+			// Switch
 			switch (opcion) {
+			
+				// Case 1: muestra la lista de viajes
 				case 1 -> {
-					// Case 1
-				}
+					System.out.println(gv.listaViajes.toString());
+				} // Fin Case 1
 				
+				// Case 2: añade un viaje
 				case 2 -> {
-					// Case 2
-				}
+					
+					// Pide al usuario un lugar
+					System.out.println("Inserta un lugar");
+					lugar = sc.nextLine();
+					
+					// Pide al usuario una fecha
+					System.out.println("Inserta una fecha");
+					fecha = sc.nextLine();
+					
+					// Try-Catch: Pide al usuario un precio
+					try {
+						System.out.println("Inserta un precio");
+						precio = sc.nextInt();
+					} catch (InputMismatchException e) {
+						System.out.println("ERROR: respuesta invalida");
+					} finally {
+						sc.nextLine();
+					} // Fin Try-Catch
+					
+					// Añade el viaje
+					gv.agregarViaje(new Viaje(lugar, fecha, precio));
+					
+				} // Fin Case 2
 				
+				// Case 3: elimina un viaje (WIP)
 				case 3 -> {
-					// Case 3
-				}
+					System.out.println("ERROR: opcion aun no disponible");
+				} // Fin Case 3
 				
+				// Case 4: actualiza un viaje (WIP)
 				case 4 -> {
-					// Case 4
-				}
-			}
-		} while (opcion != 5);
+					System.out.println("ERROR: opcion aun no disponible");
+				} // Fin Case 4
+				
+			} // Fin Switch
+		} while (opcion != 5); // Fin Do-While
 		
+		// Muestra un mensaje cerrando el programa
 		System.out.println("Cerrando programa...");
 		
-		// Aqui se tendria que mandar al programa a que actualice el fichero
+		// Guarda los archivos en un documento nuevo
+		gv.guardar();
 		
+		// Cierra el Scanner
 		sc.close();
 		
 	}
 	
+	/**
+	 * Funcion que muestra el menu
+	 */
 	private static void mostrarMenu() {
 		System.out.println("Programa de gestion de viajes");
 		System.out.println("---------------------------------");
